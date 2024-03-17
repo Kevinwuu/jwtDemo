1fe87a251df1b88d55d7b420aea4ae437342ced0
Ethan Wu <405417048@gms.tku.edu.tw>
2024-03-17 19:41:20 +0800

    fix bug

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
+var cors = require('cors');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
