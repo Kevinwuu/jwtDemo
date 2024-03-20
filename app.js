8a55a7d2ea579c41585392fdf787ad07806326a0
Ethan Wu <405417048@gms.tku.edu.tw>
2024-03-20 10:07:32 +0800

    test

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-// var cors = require('cors');
 
 var app = express();
-// app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
